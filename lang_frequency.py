475a13494d162c4073cd177e4ce8bbec784e48d6
a.leskin <a.leskin@a.leskin>
2018-03-14 22:56:28 +0300

    fixed comments

--- lang_frequency.py
+++ lang_frequency.py
@@ -6,21 +6,24 @@ import collections
 def load_data(filepath):
     with open(filepath, 'r') as file_contents:
         text_string = file_contents.read().lower()
-    all_the_words = re.findall(r'(\w+)', text_string, re.UNICODE)
-    frequency = collections.Counter(all_the_words).most_common()[:10]
+    return text_string
+
+
+def get_most_frequent_words(text_string):
+    list_of_all_the_words = re.findall(r'(\w+)', text_string, re.UNICODE)
+    frequency = collections.Counter(list_of_all_the_words).most_common(10)
     return frequency
 
 
-def get_most_frequent_words(text):
-
-    for word in text:
-        print(word[0])
+def print_top_ten_words(frequency):
+    for word, number_of_repeats in frequency:
+        print(word)
 
 
 if __name__ == '__main__':
     try:
         user_file_path = sys.argv[1]
-        text = load_data(user_file_path)
+        text_string = load_data(user_file_path)
     except (FileNotFoundError, IndexError):
         exit('Указанный файл не найден')
-    get_most_frequent_words(text)
+    print_top_ten_words(get_most_frequent_words(text_string))
